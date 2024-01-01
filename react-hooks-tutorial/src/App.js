ff11603822031655f923a58c2a74734c688ec0cc
Mamba1099 <mambakaranja240@gmail.com>
2024-01-02 00:10:06 +0300

    increment a value by 1

---
+++ react-hooks-tutorial/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(10);
+
+  return(
+    <div>
+      <button onClick = {() => setCount(count + 1)}>+</button>
+      <div>{count}</div>
+    </div>
+
+  )
+}
+
+
+
+export default App
