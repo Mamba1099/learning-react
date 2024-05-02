b403c6f037779fe9f646e22b3dc1906b186058d3
Mamba1099 <mambakaranja240@gmail.com>
2024-05-03 02:36:37 +0300

    refactor: rewrite function to generate token to avoid being redundant

--- current-project/authServer.js
+++ current-project/authServer.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 let refreshTokens = [];
 
-app.post("/token", (req, res) => { 
+app.post("/token", (req, res) => {
   const refreshToken = req.body.token;
   if (refreshToken == null) return res.sendStatus(401);
   if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
@@ -17,28 +17,28 @@ app.post("/token", (req, res) => {
     const accessToken = generateAccessToken({ name: user.name });
     res.json({ accessToken: accessToken });
   });
-})
+});
 
 app.delete("/logout", (req, res) => {
-  refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
   res.sendStatus(204);
-})
+});
 
 app.post("/login", (req, res) => {
   // Authenticate User
 
   const username = req.body.username;
   const user = { name: username };
-
-  const accessToken = generateAccessToken(user);
-  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-  refreshTokens.push(refreshToken);
-  res.json({ accessToken: accessToken, refreshToken: refreshToken});
+  const { accessToken, refreshToken } = generateTokens(user);
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 
-function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
-
+function generateTokens(user) {
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "45s",
+  });
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  return { accessToken: accessToken, refreshToken: refreshToken };
 }
 
 app.listen(4000, () => {
